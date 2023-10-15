f89de4b34b7a0a28f15fa16e39bca7f6ff9dc36c
Ayushman Singh <amansinghayush@gmail.com>
2023-10-15 18:19:43 +0530

    middleware auth added

---
+++ middlewares/authenticate.js
@@ -0,0 +1,37 @@
+const Users= require('../models/user')
+const jwt= require('jsonwebtoken')
+
+const auth= async (req,res,next)=>{
+	
+	const authHeader= req.Headers.token
+
+	if(!authHeader || !authHeader.startsWith('Bearer '))
+		res.status(403).json("Invalid Authentication")
+
+	else
+	{
+		const token= authHeader.split(" ")[1]
+		
+		try{
+		await jwt.verify(token, process.env.JWT_SECRET,(err,user)=>{
+
+			if(err)
+				res.status(403).json("Invalid Token")
+
+			else
+			{
+				req.user=user;
+
+				next();
+			}
+		})
+		}
+		catch(error){
+			console.log(error)
+			res.status(501).json("Error during Authentication")
+		}
+	}
+
+}
+
+module.exports= auth;
