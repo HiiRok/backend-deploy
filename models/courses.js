1a38897269f022dea516a5c52ecdd49f3c5f0ffd
Ayushman Singh <amansinghayush@gmail.com>
2023-10-15 15:28:45 +0530

    updated course model

--- models/courses.js
+++ models/courses.js
@@ -10,6 +10,29 @@ const courseSchema= new mongoose.Schema(
 			minlength: [1,"Name must have atleast 1 character"]
 		}
 
+		Brief_Desc: {
+			type: String,
+			required: [true, "Must provide some description"],
+			maxlength: [500,"Must not Exceed 500 characters"]
+		},
+
+		Price: {
+			type: Number,
+			required:[true, "Must provide a type"]
+		},
+
+		Author: {
+			type: String
+		},
+
+		//Objectives are array of strings. 
+		Objectives: {
+			type: Array,
+			required: [true, "Must provide Course Objectives"]
+		}
 
 	}
 )
+
+
+module.exports=mongoose.model("Courses",courseSchema)
