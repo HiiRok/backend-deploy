e5d27236c20bac46859ab72615b71948ea4cd699
Ayushman Singh <amansinghayush@gmail.com>
2023-10-15 15:08:23 +0530

    course cat model added

---
+++ models/course_cat.js
@@ -0,0 +1,15 @@
+const mongoose= require('mongoose')
+
+const courseCats= new mongoose.Schema(
+
+	{
+		Name:{
+			type: String,
+			required:[true, "Must provide a name"],
+			minlength:[1,"Name atleast one character long"],
+			unique:[true,"Name already exists"]
+		}
+	}
+)
+
+module.exports= mongoose.model('Course_Categories',courseCats);
